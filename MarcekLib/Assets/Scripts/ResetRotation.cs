f7f2aba04e8049a5a71dd629eaf6594848612f70
wtmarcek <wtmarcek@gmail.com>
2017-09-29 00:59:55 +0200

    .ignore, RotateByDrag, ResetRotation

---
+++ MarcekLib/Assets/Scripts/ResetRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetRotation : MonoBehaviour {
+
+    Quaternion initialQuaternion;
+    Vector3 initialEuler;
+
+	// Use this for initialization
+	void Start ()
+    {
+        initialQuaternion = transform.rotation;
+        initialEuler = transform.eulerAngles;
+	}
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            StartCoroutine(LerpResetRotation(1));
+        }
+    }
+
+    IEnumerator LerpResetRotation(float speed)
+    {
+        while (Mathf.Abs(Vector3.SqrMagnitude(transform.eulerAngles) - Vector3.SqrMagnitude(initialEuler)) > 5f)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, initialQuaternion, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.rotation = initialQuaternion;
+    }
+}
---
+++ MarcekLib/Assets/Scripts/RotateByDrag.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshRenderer))]
+
+public class RotateByDrag : MonoBehaviour
+{
+    public float sensitivity = 1f;
+    private Vector3 mouseReference;
+    private Vector3 mouseOffset;
+    private Vector3 rotation = Vector3.zero;
+    private bool isRotating;
+
+
+    void Update()
+    {
+        if (isRotating)
+        {
+            mouseOffset = (Input.mousePosition - mouseReference);
+
+            rotation.x = (mouseOffset.y) * sensitivity;
+            rotation.y = -(mouseOffset.x) * sensitivity;
+
+            gameObject.transform.Rotate(rotation, Space.World);
+
+            mouseReference = Input.mousePosition;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isRotating = true;
+
+        mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {               
+        isRotating = false;
+
+        StartCoroutine(RotationSmooth());
+    }
+
+    IEnumerator RotationSmooth()
+    {
+        Vector3 myRotation = rotation;
+        myRotation = Vector3.ClampMagnitude(myRotation, 10);
+
+        for (int i = 30; i >= 0; i--)
+        {
+            myRotation *= .9f;
+            transform.Rotate(myRotation, Space.World);
+
+            yield return null;
+        }
+    }
+
+}
