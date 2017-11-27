ab19d6c587d008c18ac37006af4c5d4bb3139890
wtmarcek <wtmarcek@gmail.com>
2017-11-27 19:45:33 +0100

    rotate by time

---
+++ MarcekLib/Assets/Scripts/LocalToWorld.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalToWorld : MonoBehaviour {
+
+    BoxCollider boxCollider;
+
+    public Vector3 center;
+    public Vector3 parameters;
+
+	void Start ()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+	
+	void Update ()
+    {
+        center = boxCollider.center;
+        
+        parameters = transform.TransformPoint(boxCollider.size.x, boxCollider.size.y, boxCollider.size.z);
+        print("LocalToWorld " + ToWorldCoord());
+        print("Center " + center);
+
+        Matrix4x4 cubeTransform = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+
+        //transform.rotation *= cubeTransform.rotation;
+
+    }
+
+    private Vector3 ToWorldCoord()
+    {
+        return new Vector3(center.x + boxCollider.size.x, center.y + boxCollider.size.y, center.z + boxCollider.size.z);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.matrix *= Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+
+        Gizmos.DrawCube(center, ToWorldCoord());
+    }
+}
---
+++ MarcekLib/Assets/Scripts/RotateByTime.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateByTime : MonoBehaviour
+{
+    [SerializeField] bool isDoRotation = false;
+    Vector3 startLocalEuler = new Vector3(0, 0, 0);
+
+    void Start()
+    {
+        startLocalEuler = transform.localEulerAngles;
+    }
+
+    private void Update()
+    {
+        if (isDoRotation)
+        {
+            DoRotation();
+            print("executed");
+        }
+    }
+
+    Vector3 direction = Vector3.zero;
+    void DoRotation()
+    {
+        if (direction == Vector3.zero)
+        {
+            direction = Quaternion.Euler(startLocalEuler - transform.localEulerAngles) * transform.forward;            
+        }
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(direction), 1f);
+
+        if (Vector3.Angle(startLocalEuler, transform.forward) < 0.5f)
+        {
+            transform.localEulerAngles = startLocalEuler;
+        }
+            
+
+    }
+
+
+
+}
